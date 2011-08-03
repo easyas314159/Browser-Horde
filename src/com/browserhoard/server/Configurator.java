70efb8c88b6b9accdf870ad8ebc6b068fb5d74f7
Kevin Loney <kevin.loney@gmail.com>
2011-08-02 23:12:18 -0600

    -Initial project setup

---
+++ src/com/browserhoard/server/Configurator.java
@@ -0,0 +1,82 @@
+package com.browserhoard.server;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.TimeZone;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import net.spy.memcached.AddrUtil;
+import net.spy.memcached.ConnectionFactory;
+import net.spy.memcached.DefaultConnectionFactory;
+import net.spy.memcached.MemcachedClient;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+
+import com.browserhoard.server.util.GsonUtils;
+
+public class Configurator implements ServletContextListener {
+	private final Logger log = Logger.getLogger(getClass());
+
+	public static final String MEMCAHED = "memcached";
+
+	public void contextInitialized(ServletContextEvent event) {
+		try {
+			ServletContext context = event.getServletContext();
+
+			TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+			
+			GsonUtils.getGsonBuilder()
+				;
+
+			initMemcached(context);
+		}
+		catch(Throwable t) {
+			log.error("Uh Oh!", t);
+		}
+	}
+
+	public void contextDestroyed(ServletContextEvent eventt) {
+	}
+
+	private MemcachedClient initMemcached(ServletContext context) {
+		ClassLoader loader = ConnectionFactory.class.getClassLoader();
+		String connectionFactoryClass = StringUtils
+				.trimToNull(context
+						.getInitParameter(ServletInitOptions.MEMCACHED_CONNECTION_FACTORY));
+
+		ConnectionFactory connectionFactory = null;
+		if(connectionFactoryClass != null) {
+			try {
+				connectionFactory = loader.loadClass(connectionFactoryClass)
+						.asSubclass(ConnectionFactory.class).newInstance();
+			} catch(Throwable t) {
+				log.warn("Failed to instantiate memcached connection factory", t);
+			}
+		}
+		if(connectionFactory == null) {
+			log.info("Using default memcached connection factory");
+			connectionFactory = new DefaultConnectionFactory();
+		}
+
+		MemcachedClient memcached = null;
+		String cluster = StringUtils.trimToNull(context
+				.getInitParameter(ServletInitOptions.MEMCACHED_CLUSTER));
+		if(cluster == null) {
+			log.warn("No memcached cluster specified; memcached will be disabled");
+		} else {
+			try {
+				List<InetSocketAddress> addresses = AddrUtil
+						.getAddresses(cluster);
+				memcached = new MemcachedClient(connectionFactory, addresses);
+				context.setAttribute(Configurator.MEMCAHED, memcached);
+			} catch(Throwable t) {
+				log.error("Unable to create memcached client", t);
+			}
+		}
+		return memcached;
+	}
+}
---
+++ src/com/browserhoard/server/ServletInitOptions.java
@@ -0,0 +1,6 @@
+package com.browserhoard.server;
+
+public final class ServletInitOptions {
+	public static final String MEMCACHED_CLUSTER = "memcached.cluster";
+	public static final String MEMCACHED_CONNECTION_FACTORY = "memcached.connection_factory";
+}
