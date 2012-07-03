f491da60970b7d41069e8d527f43786cd26492fd
Kevin Loney <kevin.loney@gmail.com>
2012-07-03 16:32:40 -0600

    -Started work on script creation -Refined method for passing object changes into create and update api methods

---
+++ src/com/browserhorde/server/api/consumes/ScriptObject.java
@@ -0,0 +1,12 @@
+package com.browserhorde.server.api.consumes;
+
+import java.util.HashMap;
+
+public class ScriptObject extends HashMap<String, Object> {
+	public static final String OWNER = "owner";
+
+	public static final String PUBLIC = "is_public";
+
+	public static final String NAME = "name";
+	public static final String DESCRIPTION = "description";
+}
