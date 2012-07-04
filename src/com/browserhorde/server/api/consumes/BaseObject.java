b1e7e15927140da1749c7050e4948c3de697890d
Kevin Loney <kevin.loney@gmail.com>
2012-07-04 15:55:16 -0600

    -Expanded on get*() methods in consumed BaseObject

--- src/com/browserhorde/server/api/consumes/BaseObject.java
+++ src/com/browserhorde/server/api/consumes/BaseObject.java
@@ -5,9 +5,16 @@ import java.util.HashMap;
 class BaseObject extends HashMap<String, Object> {
 	public String getString(String key) {
 		Object o = get(key);
-		if(o instanceof String) {
-			return (String)o;
-		}
-		return null;
+		return (o instanceof String) ? (String)o : null;
+	}
+
+	public Integer getInteger(String key) {
+		Object o = get(key);
+		return (o instanceof Integer) ? (Integer)o : null;
+	}
+
+	public Boolean getBoolean(String key) {
+		Object o = get(key);
+		return (o instanceof Boolean) ? (Boolean)o : null;
 	}
 }
